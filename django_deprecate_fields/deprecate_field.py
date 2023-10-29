96efbeb7dd22e6acd148bfe2b5ce32db53e68fa4
Adrian Torres <atorresj@redhat.com>
2023-10-29 23:12:10 +0100

    Add and edit copyright headers where necessary
    This is done in order to be fully compliant with the Apache license,
    this commit also adds a prominent notice to existing source files that
    have been modified.

--- django_deprecate_fields/deprecate_field.py
+++ django_deprecate_fields/deprecate_field.py
@@ -1,3 +1,22 @@
+# Copyright 2023 Adrian Torres
+# Copyright 2018-2023 3YOURMIND GmbH
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This file has been modified by Adrian Torres in order to implement new
+# features, fix bugs and improve code and documentation.
+
+
 import logging
 import sys
 import warnings
